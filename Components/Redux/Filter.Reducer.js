824c6f9e49538724ee72c841ed8851aa55e478c0
IncomReact <romain.baly@gmail.com>
2019-07-25 16:03:54 +0200

    Filter reducer

---
+++ Components/Redux/Filter.Reducer.js
@@ -0,0 +1,38 @@
+export default function ( filterData = [], action) {
+
+    var copyfilterData = [...filterData]
+    console.log('Redux ==========',action)
+
+    if (action.type == 'mood') {
+
+        copyfilterData.push(action.mood)
+
+        console.log('========= copy',copyfilterData)
+
+        return copyfilterData;
+    }
+
+    else if (action.type == 'with') {
+        copyfilterData.push(action.with)
+
+        console.log('========= copy',copyfilterData)
+
+        return copyfilterData;
+
+    }
+    
+    else if (action.type == 'type') {
+        copyfilterData.push(action.format)
+
+        console.log('========= copy',copyfilterData)
+
+        return copyfilterData;
+
+    } 
+
+    else {
+        return filterData;
+    }
+
+    
+}
